2567e72738251bc3c6a8b902b0b27c7dc9220462
Camilo Muñoz <camilomunozmunoz1@gmail.com>
2021-05-28 14:47:09 -0500

    Person development 2

--- src/sample/logic/entities/Person.java
+++ src/sample/logic/entities/Person.java
@@ -12,39 +12,47 @@ public class Person {
     private String reason;
     private String id;
     public static final int MAX_ID_DIGITS = 8;
-    public static final int MIN_ID_DIGITS = 11;
+    public static final int MIN_ID_DIGITS = 10;
 
-    public Person(String name, String lastName, int age, String sex, String department, String condition, String reason, String id) {
+    public Person(String name, String lastName, String age, String sex, String department, String condition, String reason, String id) throws PersonException {
         this.name = name;
         this.lastName = lastName;
-        this.age = age;
+        setAge(age);
         this.sex = sex;
         this.department = department;
         this.condition = condition;
         this.reason = reason;
-        this.id = id;
+        setId(id);
     }
 
     private void setAge(String inputAge) throws PersonException {
         if(inputAge==null) {
-            throw new PersonException(PersonException.MISSING_AGE_PARAMETER_);
+            throw new PersonException(PersonException.MISSING_PARAMETER);
         }
         try {
             int result = Integer.parseInt(inputAge);
             if (result> 150) throw new PersonException(PersonException.UPPER_AGE);
             if (result<= 0) throw new PersonException(PersonException.UNDER_AGE);
+            this.age = result;
         } catch (NumberFormatException e) {
             throw new PersonException(PersonException.INVALID_CHARACTERS);
         }
+
     }
     private void setId(String inputId) throws PersonException {
+        int count =0;
         if(inputId==null) {
-            throw new PersonException(PersonException.MISSING_AGE_PARAMETER_);
+            throw new PersonException(PersonException.MISSING_PARAMETER);
         }
         try {
-            int result = Integer.parseInt(inputId);
-            if (result> 150) throw new PersonException(PersonException.UPPER_AGE);
-            if (result<= 0) throw new PersonException(PersonException.UNDER_AGE);
+            Long.parseLong(inputId);
+            for (int i = 0; i < inputId.length(); i++) {
+                if(inputId.charAt(i)== 'a'){
+
+                }
+            }
+
+
         } catch (NumberFormatException e) {
             throw new PersonException(PersonException.INVALID_CHARACTERS);
         }
--- src/sample/logic/services/PersonException.java
+++ src/sample/logic/services/PersonException.java
@@ -3,8 +3,8 @@ package sample.logic.services;
 public class PersonException extends Exception{
     public static final String UNDER_AGE = "La edad introducida es menor a 0 años";
     public static final String UPPER_AGE = "La edad introducida es mayor a 150 años";
-    public static final String INVALID_CHARACTERS = "La edad indtroducida no es valida, ingrese un numero entre 1 y 150";
-    public static final String MISSING_AGE_PARAMETER_ = "El parametro de edad esta vacío";
+    public static final String INVALID_CHARACTERS = "El parametro introducido es invalido";
+    public static final String MISSING_PARAMETER = "Parametro vacío";
 
 
 
