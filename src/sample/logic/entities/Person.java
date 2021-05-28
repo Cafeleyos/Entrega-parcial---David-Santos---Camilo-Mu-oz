8e089afc8b2d3b0dd7fdea63c78a314369b53a80
Camilo Muñoz <camilomunozmunoz1@gmail.com>
2021-05-28 14:20:54 -0500

    Person development1

--- src/sample/logic/entities/Person.java
+++ src/sample/logic/entities/Person.java
@@ -1,5 +1,7 @@
 package sample.logic.entities;
 
+import sample.logic.services.PersonException;
+
 public class Person {
     private String name;
     private String lastName;
@@ -9,4 +11,44 @@ public class Person {
     private String condition;
     private String reason;
     private String id;
+    public static final int MAX_ID_DIGITS = 8;
+    public static final int MIN_ID_DIGITS = 11;
+
+    public Person(String name, String lastName, int age, String sex, String department, String condition, String reason, String id) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.sex = sex;
+        this.department = department;
+        this.condition = condition;
+        this.reason = reason;
+        this.id = id;
+    }
+
+    private void setAge(String inputAge) throws PersonException {
+        if(inputAge==null) {
+            throw new PersonException(PersonException.MISSING_AGE_PARAMETER_);
+        }
+        try {
+            int result = Integer.parseInt(inputAge);
+            if (result> 150) throw new PersonException(PersonException.UPPER_AGE);
+            if (result<= 0) throw new PersonException(PersonException.UNDER_AGE);
+        } catch (NumberFormatException e) {
+            throw new PersonException(PersonException.INVALID_CHARACTERS);
+        }
+    }
+    private void setId(String inputId) throws PersonException {
+        if(inputId==null) {
+            throw new PersonException(PersonException.MISSING_AGE_PARAMETER_);
+        }
+        try {
+            int result = Integer.parseInt(inputId);
+            if (result> 150) throw new PersonException(PersonException.UPPER_AGE);
+            if (result<= 0) throw new PersonException(PersonException.UNDER_AGE);
+        } catch (NumberFormatException e) {
+            throw new PersonException(PersonException.INVALID_CHARACTERS);
+        }
+    }
+
+
 }
