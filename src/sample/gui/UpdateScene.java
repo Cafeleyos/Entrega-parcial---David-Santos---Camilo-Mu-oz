e036a9594ca51fae07e041d70c391e65c968bc86
Camilo Muñoz <camilomunozmunoz1@gmail.com>
2021-06-02 17:08:59 -0500

    UpdateScene

--- src/sample/gui/UpdateScene.java
+++ src/sample/gui/UpdateScene.java
@@ -1,6 +1,57 @@
 package sample.gui;
 
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import sample.logic.services.implementation.PersonaServices;
+
 public class UpdateScene {
+    private Button buttonAdd, buttonCancel;
+    private final Stage stage;
+    private Scene addScene;
+    private TextField inputName, inputLastname, inputAge, inputReason, inputId;
+    private ComboBox<String> inputDepartment, inputSex, inputCondition, inputPosition;
+    private Label name, lastname, age, sex, department, condition, reason, id, position;
+    private GridPane pane;
 
+    private static final Text TITLE = new Text("Modificar Persona");
 
+    private PersonaServices personaServices;
+
+    public UpdateScene(PersonaServices personaServices) {
+        stage = new Stage();
+        this.personaServices = personaServices;
+        setUp();
+        behavior();
+
+        stage.setTitle("Modificar Persona");
+        stage.setScene(addScene);
+        stage.show();
+    }
+
+    public void setUp() {
+        setUpPane();
+        addScene = new Scene(pane, 400, 550);
+    }
+
+    public void behavior() {
+
+    }
+
+    public void setUpPane() {
+        pane = new GridPane();
+        pane.setAlignment(Pos.CENTER);
+        pane.setHgap(20);
+        pane.setVgap(20);
+
+        TITLE.setFont(DataScene.FONT_TITLE);
+        pane.add(TITLE, 0, 0, 2, 1);
+
+    }
 }
