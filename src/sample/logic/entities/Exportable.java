7a23b78d7b7f155dd72ce0aa0c67ccda4684cd52
Camilo Muñoz <camilomunozmunoz1@gmail.com>
2021-06-04 11:47:01 -0500

    doc - exportable

--- src/sample/logic/entities/Exportable.java
+++ src/sample/logic/entities/Exportable.java
@@ -5,6 +5,9 @@ import sample.gui.SetUp;
 
 import java.util.List;
 
+/**
+ * Class that standardizes with objects can be exported.
+ */
 public abstract class Exportable {
 
     public static final Character CSV = ',';
@@ -13,8 +16,18 @@ public abstract class Exportable {
 
     public abstract List<String> toStringList();
 
+    /**
+     * method for getting the header of the object
+     * @param separateCharacter for the string
+     * @return string with the header separated with the given character
+     */
     public abstract String getHeader(Character separateCharacter);
 
+    /**
+     * Given a Character, it returns the extension.
+     * @param characterSeparate for checking
+     * @return the extension
+     */
     public static String getExtension(Character characterSeparate) {
         return switch (characterSeparate) {
             case ',' -> "csv";
@@ -24,6 +37,11 @@ public abstract class Exportable {
         };
     }
 
+    /**
+     * Gets a list of exportable objects and converts them into an specific extension
+     * @param characterSeparate of the extension
+     * @return String separated by the character given
+     */
     public String toExportValue(Character characterSeparate) {
         int count = 0;
         List<String> stringList = this.toStringList();
