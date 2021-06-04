f9469fdda5069cf776335a28f240fe4d322361f7
Camilo Muñoz <camilomunozmunoz1@gmail.com>
2021-06-04 17:45:49 -0500

    doc - confirmation scene

--- src/sample/gui/ConfirmationScene.java
+++ src/sample/gui/ConfirmationScene.java
@@ -8,20 +8,27 @@ import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+/**
+ * window for checking other scene actions
+ */
 public class ConfirmationScene extends Stage {
 
     private Scene confirmationScene;
     private Stage stage;
-
-    private static final Text EDIT = new Text("¿Estás seguro que quieres editar está persona?");
-    private static final Text ADD = new Text("¿Estás seguro que quieres agregar está persona?");
-    private static final Text DELETE = new Text("¿Estás seguro que quieres eliminar está persona?");
     private String mode;
     private GridPane pane;
-
     private Button confirmation, cancel;
     private boolean result;
 
+    private static final Text EDIT = new Text("¿Deseas editar a ésta persona?");
+    private static final Text ADD = new Text("¿Deseas agregar a ésta persona?");
+    private static final Text DELETE = new Text("¿Deseas eliminar a ésta persona?");
+
+    /**
+     * Constructor for creating a ConfirmationScene instance
+     * @param ownerStage of the main window
+     * @param mode "Agregar", "Editar" or "Eliminar"
+     */
     public ConfirmationScene(Stage ownerStage, String mode) {
         stage = new Stage();
         this.mode = mode;
@@ -36,6 +43,9 @@ public class ConfirmationScene extends Stage {
         stage.showAndWait();
     }
 
+    /**
+     * Method that calls other sub methods for setting up the report stage and scene.
+     */
     public void setUp() {
         setUpButton();
         setUpPane();
@@ -43,6 +53,9 @@ public class ConfirmationScene extends Stage {
         confirmationScene = new Scene(pane, 300, 100);
     }
 
+    /**
+     * Gives action to all the elements present in the scene.
+     */
     public void behavior() {
         confirmation.setOnAction(e -> {
             stage.close();
@@ -55,6 +68,9 @@ public class ConfirmationScene extends Stage {
         });
     }
 
+    /**
+     * sets up all the buttons.
+     */
     public void setUpButton() {
         confirmation = new Button("Confirmar");
         confirmation.setPrefSize(100, 30);
@@ -63,6 +79,9 @@ public class ConfirmationScene extends Stage {
         cancel.setPrefSize(100, 30);
     }
 
+    /**
+     * Creates and sets up the Grid pane that contains the text and the buttons. Also defines which mode is going to be used
+     */
     public void setUpPane() {
         pane = new GridPane();
         pane.setAlignment(Pos.CENTER);
@@ -78,8 +97,9 @@ public class ConfirmationScene extends Stage {
         pane.add(confirmation, 0, 1);
         pane.add(cancel, 1, 1);
     }
-
+    
     public boolean getConfirmation() {
         return result;
     }
+
 }
