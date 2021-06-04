8e0718bdf59344a44bd12328d2d63c1100b6b956
Camilo Muñoz <camilomunozmunoz1@gmail.com>
2021-06-04 14:12:42 -0500

    Update PersonaPersistence.java

--- src/sample/logic/persistence/implementation/PersonaPersistence.java
+++ src/sample/logic/persistence/implementation/PersonaPersistence.java
@@ -7,6 +7,9 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class that allows to persist information of a person.
+ */
 public class PersonaPersistence implements IPersonaPersistence, Serializable{
     private static final String PEOPLE_FILE_PATH = "people.database";
     private File file;
@@ -14,6 +17,9 @@ public class PersonaPersistence implements IPersonaPersistence, Serializable{
     private ObjectOutputStream write;
     private List<Persona> data;
 
+    /**
+     * Constructor for creating an instance. It creates the database file if it doesn't exist.
+     */
     public PersonaPersistence() throws IOException {
         file = new File(PEOPLE_FILE_PATH);
         if (file.createNewFile()) {
@@ -22,6 +28,11 @@ public class PersonaPersistence implements IPersonaPersistence, Serializable{
         data = new ArrayList<>();
     }
 
+    /**
+     * Method for saving a person. If the boolean value is true, it adds but if it´s false, it deletes.
+     * @param persona to save
+     * @param option to delete or add.
+     */
     @Override
     public void save(Persona persona, boolean option) throws IOException {
         if (option) {
@@ -32,7 +43,10 @@ public class PersonaPersistence implements IPersonaPersistence, Serializable{
         write();
     }
 
-
+    /**
+     * Method for reading the objects of the file where they are being saved.
+     * @return list of people that was read.
+     */
     @Override
     public List<Persona> read() throws IOException, ClassNotFoundException {
             if(file.length() != 0) {
@@ -43,12 +57,19 @@ public class PersonaPersistence implements IPersonaPersistence, Serializable{
         return data;
     }
 
+    /**
+     * Method for replacing an specific person with a new one in the database file.
+     * @param newPersona to add
+     */
     @Override
     public void replace(Persona newPersona,Persona personaToReplace) throws IOException {
             data.set(data.indexOf(personaToReplace),newPersona);
             write();
     }
 
+    /**
+     * Method that overwrites the "data" list in the database file.
+     */
     @Override
     public void write() throws IOException {
         write = new ObjectOutputStream(new FileOutputStream(PEOPLE_FILE_PATH));
