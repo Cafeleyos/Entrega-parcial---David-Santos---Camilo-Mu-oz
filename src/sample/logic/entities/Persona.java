a27700bcc682832527259dc9ba2f097c2d0384a6
Camilo Muñoz <camilomunozmunoz1@gmail.com>
2021-06-04 11:59:33 -0500

    doc - Persona

--- src/sample/logic/entities/Persona.java
+++ src/sample/logic/entities/Persona.java
@@ -8,6 +8,9 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class that represents a person affected by the national stoppage
+ */
 public class Persona extends Exportable implements Serializable {
     protected String name;
     protected String lastName;
@@ -23,6 +26,11 @@ public class Persona extends Exportable implements Serializable {
     public static final int MAX_AGE = 150;
     public static final int MIN_AGE = 1;
 
+    /**
+     * Initializes a person
+     * @param reason why it was a victim
+     * @throws PersonaException if there is a wrong parameter
+     */
     public Persona(String name, String lastName, String age, String sex, String department, String condition, String reason, String id) throws PersonaException {
         this.name = name;
         this.lastName = lastName;
@@ -60,6 +68,11 @@ public class Persona extends Exportable implements Serializable {
         return this.id;
     }
 
+    /**
+     * Method for setting the age that takes into account that the given one have a value between the min and max age.
+     * @param inputAge for setting
+     * @throws PersonaException if the age is not valid
+     */
     private void setAge(String inputAge) throws PersonaException {
         try {
             int result = Integer.parseInt(inputAge);
@@ -72,6 +85,12 @@ public class Persona extends Exportable implements Serializable {
 
     }
 
+    /**
+     * Method for setting the id that takes into account that the given value is between the established
+     * max and min values.
+     * @param inputId to setup
+     * @throws PersonaException if the id isnt valid
+     */
     private void setId(String inputId) throws PersonaException {
         try {
             Long.parseLong(inputId);
@@ -83,6 +102,10 @@ public class Persona extends Exportable implements Serializable {
         }
     }
 
+    /**
+     * Converts the attributes of the class into a string
+     * @return string
+     */
     @Override
     public String toString() {
         return String.format("Nombre Completo: " + name + " " + lastName + "\n"
@@ -94,6 +117,10 @@ public class Persona extends Exportable implements Serializable {
                 + "Razón: " + reason + "\n");
     }
 
+    /**
+     * String list of the attributes of the class
+     * @return String list
+     */
     @Override
     public List<String> toStringList() {
         List<String> result = new ArrayList<>();
@@ -111,6 +138,11 @@ public class Persona extends Exportable implements Serializable {
         return result;
     }
 
+    /**
+     * sets the header for all the attributes of the class
+     * @param separateCharacter for splitting the string
+     * @return header string
+     */
     @Override
     public String getHeader(Character separateCharacter) {
         return SetUp.NAME + separateCharacter + SetUp.LAST_NAME + separateCharacter + SetUp.AGE + separateCharacter + SetUp.AGE +
